9353e775cca5660aae6593d5d41f5b9103dfef5d
devsdenepal <gautamn7822@gmail.com>
2023-04-27 22:21:17 +0545

    1

--- ch.sh
+++ ch.sh
@@ -1,1 +1,14 @@
 # contributor automater init worked...now aiming to create a ssame to check the file and commit on any change
+#!/bin/bash
+contribution=0
+while true
+do
+  ((contribution++))
+ # add your command here
+  echo "Running command..."
+  echo $contribution>cont.txt
+  git add *
+  git commit -m $contribution
+  git push -u origin HEAD
+  # wait for 5 seconds before running the command again
+done
