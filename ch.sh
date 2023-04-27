7b58ca8a892d35fcb95b1f7a4fce853cf293d661
devsdenepal <gautamn7822@gmail.com>
2023-04-27 21:27:17 +0545

    1

---
+++ ch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+contribution=0
+while true
+do
+  ((contribution++)) 
+ # add your command here
+  echo "Running command..."
+  echo $contribution>cont.txt
+  git add *
+  git commit -m $contribution
+  git push -u origin HEAD  
+  # wait for 5 seconds before running the command again
+  sleep 5
+done
